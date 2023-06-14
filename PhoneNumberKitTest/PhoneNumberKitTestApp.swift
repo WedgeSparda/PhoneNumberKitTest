b284204d8926d0cbf25e19dfc4aadd627068749f
Roberto Pastor <roberto.pastor@cabify.com>
2023-06-14 09:04:57 +0200

    Initial Commit

---
+++ PhoneNumberKitTest/PhoneNumberKitTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  PhoneNumberKitTestApp.swift
+//  PhoneNumberKitTest
+//
+//  Created by Roberto Pastor on 14/6/23.
+//
+
+import SwiftUI
+
+@main
+struct PhoneNumberKitTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
