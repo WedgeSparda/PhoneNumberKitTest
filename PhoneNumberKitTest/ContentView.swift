a3caa4d69f3840a1b23612a25941605b03df0201
Roberto Pastor <roberto.pastor@cabify.com>
2023-06-14 09:17:48 +0200

    Initial commit

--- PhoneNumberKitTest/ContentView.swift
+++ PhoneNumberKitTest/ContentView.swift
@@ -6,14 +6,36 @@
 //
 
 import SwiftUI
+import PhoneNumberKit
+
+extension PhoneNumberKit {
+    public static var instance: PhoneNumberKit = {
+        return PhoneNumberKit()
+    }()
+}
+
+class ContentViewModel {
+     
+    func validatePhone() {
+        let phone = "+34 666666666"
+        do {
+            _ = try PhoneNumberKit.instance.parse(phone, ignoreType: true)
+            print("\(phone) is a VALID phone")
+        } catch {
+            print("\(phone) is an INVALID phone:", error)
+        }
+    }
+}
 
 struct ContentView: View {
+    
+    let model = ContentViewModel()
+    
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+            Button("Validate phone") {
+                model.validatePhone()
+            }
         }
         .padding()
     }
